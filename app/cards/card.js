55c4130f5169c99eaddf7925e5f30f67e70ca7db
GMostert <Momentum1>
2016-06-15 12:43:09 +0200

    Added SystemJS as module loader. Converted js files into different module formats.

---
+++ app/cards/card.js
@@ -0,0 +1,25 @@
+/* Global module */
+//function Card(card) {
+
+/* CJS module */
+//module.exports = Card;
+//function Card(card) {
+
+/* ES6 module */
+export default function Card(card) {
+    this.suit = card.suit;
+    this.rank = card.rank;
+    this.image = (Card.ranksInImagesOrder.indexOf(this.rank) * 4 + Card.suitsInImagesOrder.indexOf(this.suit) + 1) + ".png";
+    this.color = card.suit === "Spades" || card.suit === "Clubs" ? "black" : "red";
+    this.turnedUp = false;
+}
+
+Card.ranksInImagesOrder = ["Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2"];
+Card.suitsInImagesOrder = ["Clubs", "Spades", "Hearts", "Diamonds"];
+
+Card.prototype.turnUp = function () {
+    this.turnedUp = true;
+};
+Card.prototype.turnDown = function () {
+    this.turnedUp = false;
+};
